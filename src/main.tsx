d43510a6046f8fd585d3d22b4f066f081ea68f56
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-14 18:38:15 -0400

    created app pages

--- src/main.tsx
+++ src/main.tsx
@@ -21,5 +21,5 @@ createRoot(document.getElementById("root")!).render(
         </GoogleOAuthProvider>
       </AuthProvider>
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode> 
 );
---
+++ src/pages/DetailTripPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailTripPage: React.FC = () => {
+  return <div>DetailTripPage</div>;
+};
+
+export default DetailTripPage;
