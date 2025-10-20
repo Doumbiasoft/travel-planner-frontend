b9423b75622d50aa2d0576477cf9d3b9d5a33346
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-20 13:44:02 -0400

    Changed the app page loader text color

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -4,10 +4,8 @@ import { LoadingOutlined } from "@ant-design/icons";
 const PageLoader = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <Spin
-        indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />}
-      />
-      <div className="text-gray-600 text-base">Loading...</div>
+      <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+      <div className="text-white text-base">Loading...</div>
     </div>
   );
 };
